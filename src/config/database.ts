e41095d4ed1302a65f86da1c66ef7a5f646b60bd
ErianVT <erianvieira.tome@yahoo.com>
2026-03-24 06:53:30 -0300

    fix: removi o @types/pg e resolvendo o erro de build do deploy tirando pool

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,14 +2,15 @@
 
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
-import { Pool } from "pg";
+
+//import { Pool } from "pg";
 import "dotenv/config";
 
 
 const connectionString = String(process.env.DATABASE_URL);
 
-const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool);
+//const pool = new Pool({ connectionString })
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
